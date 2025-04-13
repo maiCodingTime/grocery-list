753f0d2d5fd736a5404fd1809a3fc4a624197f98
maiXcodecreations <maivue.x26@gmail.com>
2025-04-12 20:24:32 -0500

    Added delete functionality
    Added delete item functionality to HTML, CSS, and JS

--- index.js
+++ index.js
@@ -3,15 +3,27 @@
 
 
 const mainDiv = document.getElementById("main");
-const sliderPg = document.getElementById("sliderPg");
+const slideAdd = document.getElementById("slideAdd");
+const slideDelete = document.getElementById("slideDelete");
 const displayItem = document.getElementById("displayItem");
 let listArray = [];
 
 
 function showAddItemSlide(){
     mainDiv.classList.add('slideMain');
-    const changeMain = setTimeout(changeMainSlide, 2800);
-    sliderPg.classList.remove('slideAddingPage');
+    slideAdd.classList.remove('slidePage');
+    const changeMain = setTimeout(changeToAddSlide, 2800);
+}
+function showDeleteItemSlide(){
+    mainDiv.classList.add('slideMain');
+    slideDelete.classList.remove('slidePage');
+    const changeToMain = setTimeout(changeToDeleteSlide, 2800);
+}
+function backToList(){
+    slideAdd.classList.add('slidePage');
+    slideDelete.classList.add('slidePage');
+    mainDiv.classList.remove('slideMain');
+    const changePage = setTimeout(changeToMainSlide, 2800);
 }
 function addGroceryItem(){
 
@@ -48,21 +60,20 @@ function addGroceryItem(){
             alert(`${newItem} already exists in the list!`);
             document.getElementById("newItem").value = ""; 
         }
-
-        
-
     }
 }
-function backToList(){
-    sliderPg.classList.add('slideAddingPage');
-    mainDiv.classList.remove('slideMain');
-    const changeAddPg = setTimeout(changeSliderPg, 2800);
-}
-function changeMainSlide(){
+function changeToAddSlide(){
     mainDiv.style.display = "none";
-    sliderPg.style.display = "block";
+    slideDelete.style.display = "none";
+    slideAdd.style.display = "block";
 }
-function changeSliderPg(){
+function changeToMainSlide(){
     mainDiv.style.display = "block";
-    sliderPg.style.display = "none";
+    slideAdd.style.display = "none";
+    slideDelete.style.display = "none";
+}
+function changeToDeleteSlide(){
+    mainDiv.style.display = "none";
+    slideAdd.style.display = "none";
+    slideDelete.style.display = "block";
 }
